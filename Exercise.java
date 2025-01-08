772683c7fcc8d0a8c4693e44fd55128b0bf4b7ea
Shrinivas <shrinivas_pednekar@persistent.com>
2025-01-08 21:56:10 +0530

    First program

---
+++ Exercise.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+
+public class Exercise {
+
+ public static void main (String[] args) {
+	 
+   ArrayList<String> a = new ArrayList<String>();
+   
+   a.add("apple");
+   a.add("banana");
+   a.add("cherry");
+   a.add("mango");
+   a.add("apple");
+   System.out.println(a);
+   
+}
+}
